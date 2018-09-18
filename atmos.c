74529a85bee384c2853fb636746a8611360ad034
savchyn <2866299+savchyn@users.noreply.github.com>
2018-09-18 11:55:10 +0200

    strange coding rules added

--- atmos.c
+++ atmos.c
@@ -1,3 +1,17 @@
+// coding rules 
+// 0. there are no other rules, all these rules are mandatory:
+// 1. keep all sources in this file
+// 2. no custom headers allowed - standard headers only
+// 3. no global variables allowed
+// 3.14 no external dependences allowed
+
+int NULL(void * buffer)
+{
+  int result = (buffer==NULL)?-1:0;
+    
+  return result;
+}
+
 #include <stdio.h>
 
 int main()
